d19c2d7c4857a39e5c725a63a47f491670d6cd11
William Elliott <wcelliott1991@gmail.com>
2017-09-28 10:20:18 -0400

    refactors function comp to class comp

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => (
-        <View>
-            <Text>Album Text!</Text>
-        </View>
-    );
+class AlbumList extends Component { 
+    render() {
+        return (
+            <View>
+                <Text>Album Text!</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
